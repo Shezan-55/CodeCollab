f340b64e47f82a61d405e57b23db99768c73570d
AyushD95 <ayush.dahiwale95@gmail.com>
2025-04-24 07:05:00 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/profile", profileRoute);
 app.get("/", (req,res)=>{
-  res.json("Site is live")
+  res.send("Site is live")
 });
 
 mongoConnect(process.env.MONGO_URL);
